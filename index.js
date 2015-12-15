032bf981dce773e6b9cb3950d3e2023023d601be
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-12-15 02:23:22 -0800

    only ping between start and end times

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 
 var request = require('request');
 
-
 module.exports = function herokuSelfPing(url, options) {
   if(!options) {
     options = {};
   }
+
+  var wakeUpTime = (options.start || '6:00').split(':').map(function (i) {
+    return parseInt(i, 10);
+  });
+  var sleepTime = (options.end || '22:00').split(':').map(function (i) {
+    return parseInt(i, 10);
+  });
+
+  var wakeUpOffset = (60 * wakeUpTime[0] + wakeUpTime[1]) % (60 * 24);
+  var awakeMinutes = (60 * (sleepTime[0] + 24) + sleepTime[1] - wakeUpOffset) % (60 * 24);
 
   options.interval = options.interval || 20 * 1000 * 60;
   options.logger = options.logger || console.log;
@@ -24,11 +33,19 @@ module.exports = function herokuSelfPing(url, options) {
     return false;
   }
 
-
   options.verbose && options.logger("heroku-self-ping: Setting up hearbeat to " + url + " every " + options.interval + "ms.");
 
   return setInterval(function() {
-    options.logger("heroku-self-ping: Sending hearbeat to " + url);
-    request(url, function () {});
+    var now = new Date()
+    var elapsedMinutes = (60 * (now.getHours() + 24) + now.getMinutes() - wakeUpOffset) % (60 * 24)
+
+    if (elapsedMinutes < awakeMinutes) {
+      options.logger("heroku-self-ping: Sending hearbeat to " + url);
+      request(url, function (err) {
+        if (err) options.logger.error("keepalive pong:", err);
+      })
+    } else {
+      options.logger.info("Skipping keep alive, time to rest");
+    }
   }, options.interval);
 };
